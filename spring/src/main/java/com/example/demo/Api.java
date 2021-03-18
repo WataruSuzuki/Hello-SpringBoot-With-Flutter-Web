69321f44f4d6f0ce2036f04a78a0f71ac465672e
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-03-18 22:10:00 +0900

    Created application starter

---
+++ spring/src/main/java/com/example/demo/Api.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Api.class, args);
+    }
+}
