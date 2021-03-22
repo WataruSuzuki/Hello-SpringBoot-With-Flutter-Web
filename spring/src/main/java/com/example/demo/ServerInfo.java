3142152beeba12c57c293ba148e4e2ca88cb2acc
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-03-22 22:10:00 +0900

    Create the DTO objects

---
+++ spring/src/main/java/com/example/demo/ServerInfo.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class ServerInfo {
+
+    public final String name;
+
+    public final List<ServiceInfo> services;
+
+    public ServerInfo(String name, List<ServiceInfo> services) {
+        this.name = name;
+        this.services = services;
+    }
+}
